66eb093eac56581ac5b532e348944cc98101b832
anthony-propuestas <anthony.propuestas@gmail.com>
2026-03-21 02:06:45 -0300

    solucion problema con cliente id

--- .wrangler/tmp/bundle-0A13vp/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-0A13vp/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\dell\\Desktop\\New Coders\\Web New Coders\\.wrangler\\tmp\\bundle-203P7k\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\dell\\Desktop\\New Coders\\Web New Coders\\.wrangler\\tmp\\bundle-0A13vp\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\dell\\Desktop\\New Coders\\Web New Coders\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\Users\\dell\\Desktop\\New Coders\\Web New Coders\\.wrangler\\tmp\\bundle-203P7k\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\Users\\dell\\Desktop\\New Coders\\Web New Coders\\.wrangler\\tmp\\bundle-0A13vp\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\Users\\dell\\Desktop\\New Coders\\Web New Coders\\.wrangler\\tmp\\bundle-203P7k\\middleware-insertion-facade.js";
+export * from "C:\\Users\\dell\\Desktop\\New Coders\\Web New Coders\\.wrangler\\tmp\\bundle-0A13vp\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
